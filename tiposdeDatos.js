1dffe3d4ec3149365235e55f4327a4f9ac7e8387
Andrea Hernandez <andreajhl29@gmail.com  git config --global user.name Andrea Hernandez>
2023-05-08 21:27:09 -0300

    control structures

---
+++ tiposdeDatos.js
@@ -0,0 +1,52 @@
+// console.log(10 - 3);
+
+// ------ Tipos de comilla -----
+// dobles ""
+// simples ''
+// backtick o template string ``
+
+
+//------------------------ Objectos ----------------------
+
+//----------------- String ------------
+let nombre = 'Pepe Argento';
+let nombre2 = "Pepe Argento";
+
+// console.log(nombre2);
+
+
+//----------------- Numbers ------------
+let edad = 54;
+let minimo = -100
+
+// console.log('edad', edad);
+// console.log('minimo', minimo);
+
+//----------------- Boolean ------------
+let verdadero = true;
+let falso = false;
+
+// console.log(verdadero, falso);
+
+//----------------- Arreglos ------------
+let frutas = ['manzana', 'pera', 'banana']; // --> arreglo de strings
+
+// console.log(frutas);
+
+let edades = [24, 34, 44]; // --> arreglo de numbers
+
+// console.log(edades);
+
+let cualquierCosa = [24, 'pera', true]; // --> arreglo de mixmo
+
+// console.log(cualquierCosa);
+
+//----------------- Objectos ------------
+
+let persona = {
+  nombre: 'jose',
+  edad: 32,
+  altura: 1.60,
+};
+
+console.log(persona);
