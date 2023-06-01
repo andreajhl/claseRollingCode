fdc15fc5f7a07b07e099f8f7d8068024d8c65578
Andrea Hernandez <andreajhl29@gmail.com  git config --global user.name Andrea Hernandez>
2023-05-31 21:46:02 -0300

    class 31/05

---
+++ clase-31-05/index.js
@@ -0,0 +1,65 @@
+
+let carritoVerduleria = ['pera', 'manzana', 'banana'];
+
+function getCarritoLocal() {
+  let carrito = localStorage.getItem('carrito');
+  return JSON.parse(carrito);
+}
+
+function setCarritoLocal(carrito) {
+  let carritoJSON = JSON.stringify(carrito);
+
+  return localStorage.setItem('carrito', carritoJSON);
+}
+
+if (localStorage.getItem('carrito') === null) {
+  
+}
+
+function agregarFruta(fruta) {
+  let carrito = getCarritoLocal();
+  
+  carrito.push(fruta);
+  setCarritoLocal(carrito)
+}
+
+function eliminarUltimaFruta() {
+  let carrito = getCarritoLocal();
+
+  carrito.pop()
+  setCarritoLocal(carrito)
+}
+
+function encontrarFruta(fruta) {
+  let carrito = getCarritoLocal();
+
+  let item = carrito.find(function(element) {
+    if(element === fruta) return element
+  });
+
+  if(item) return 'Ya agrego esa fruta';
+  else return 'aun no agrego esa fruta'
+}
+
+function calcularTotal() {
+  let carrito = getCarritoLocal();
+
+  if(carrito.length > 0){
+    return `Tu carrito tiene ${carrito.length} items`
+  }else {
+    return 'tu carrito esta vacio'
+  }
+}
+
+function listarCarrito() {
+  let carrito = getCarritoLocal();
+  return carrito.join(', ')
+}
+
+function eliminarCarrito() {
+  localStorage.removeItem('carrito')
+}
+
+function vaciarCarrito() {
+  localStorage.setItem('carrito', JSON.stringify([]));
+}
